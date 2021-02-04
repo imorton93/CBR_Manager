e6b8fbcf504a8aa8fcf18cf1f7a7d5df6bf92260
Danielle Nix <dnix@sfu.ca>
2021-02-03 20:47:31 -0800

    Create activities.

---
+++ app/src/main/java/com/example/cbr_manager/NewVisitActivity.java
@@ -0,0 +1,14 @@
+package com.example.cbr_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewVisitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_visit);
+    }
+}
