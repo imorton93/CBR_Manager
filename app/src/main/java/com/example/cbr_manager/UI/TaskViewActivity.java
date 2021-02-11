884f99ccebdfb55c681ae7adf9368190e75025e0
Danielle Nix <dnix@sfu.ca>
2021-02-10 18:12:52 -0800

    Fix action bar errors.

--- app/src/main/java/com/example/cbr_manager/UI/TaskViewActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/TaskViewActivity.java
@@ -2,6 +2,7 @@ package com.example.cbr_manager.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ public class TaskViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_view);
-        getActionBar().hide();
+
         clickImage();
     }
 
