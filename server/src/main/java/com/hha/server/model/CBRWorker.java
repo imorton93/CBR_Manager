f145da905d0b43351d09214d000e4848247cbe87
Rakim Zubair <rakimz@sfu.ca>
2021-02-22 03:49:43 -0800

    modified server database to match android local database

--- server/src/main/java/com/hha/server/model/CBRWorker.java
+++ server/src/main/java/com/hha/server/model/CBRWorker.java
@@ -7,14 +7,14 @@ public class CBRWorker {
     private String password;
     private int id;
 
-    public CBRWorker(){
-    }
-
     public CBRWorker(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+    }
+
+    public CBRWorker(){
     }
 
     public String getFirstName() {
