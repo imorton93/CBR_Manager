defa3d238dc1a6c540bab9f6e00f3ac0b76c3401
imorton <imorton@sfu.ca>
2021-03-01 10:40:55 -0800

    New Referral Views setup in NewReferralActivity.java

--- app/src/main/java/com/example/cbr_manager/UI/NewReferralActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/NewReferralActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.cbr_manager.Forms.DisplayFormPage;
+import com.example.cbr_manager.Forms.FormPage;
 import com.example.cbr_manager.R;
 
+import java.util.ArrayList;
+
 public class NewReferralActivity extends AppCompatActivity {
+    LinearLayout form;
+    int currentPage;
+    int pageCount;
+    int imagePage;
+    ArrayList<FormPage> pages;
+    Button next;
+    Button back;
+    ProgressBar progressBar;
+    TextView progressText;
 
     public static Intent makeIntent(Context context) {
         Intent intent =  new Intent(context, NewReferralActivity.class);
@@ -22,6 +40,102 @@ public class NewReferralActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_referral);
         ToolbarButtons();
+
+        //Button setup
+        next = (Button) findViewById(R.id.nextBtnVisit);
+        next.setBackgroundColor(Color.parseColor("#6661ED24"));
+        back = (Button) findViewById(R.id.backBtn);
+
+        form = (LinearLayout) findViewById(R.id.form);
+        progressBar = (ProgressBar) findViewById(R.id.formProgress);
+        progressText = (TextView) findViewById(R.id.formProgressText);
+
+        currentPage = 1;
+
+
+        next.setOnClickListener(v -> {
+//            if (currentPage == 11) {
+//                insertClient();
+//            }
+//
+//            //make sure all required fields are filled in the page
+//            else if(!requiredFieldsFilled(pages.get(currentPage - 1))){
+//                requiredFieldsToast();
+//            }
+//            else if(currentPage < pageCount - 1){
+//                if(currentPage == 1){
+//                    back.setClickable(true);
+//                    back.setVisibility(View.VISIBLE);
+//
+//                    back.setBackgroundColor(Color.parseColor("#6661ED24"));
+//
+//
+//                }
+//                //save answers
+//                savePage(pages.get(currentPage - 1));
+//
+//                currentPage++;
+//                setProgress(currentPage, pageCount);
+//
+//                clearForm();
+//
+//                if(currentPage == imagePage){
+//                    displayPicture(pages.get(currentPage - 1));
+//                }
+//                else{
+//                    DisplayFormPage.displayPage(pages.get(currentPage - 1), form, NewClientActivity.this);
+//
+//                }
+//
+//                //load previously saved answers if any
+//                loadAnswers(pages.get(currentPage - 1));
+//
+//            }
+//            else if(currentPage == pageCount - 1){
+//                //save answers
+//                savePage(pages.get(currentPage - 1));
+//                currentPage++;
+//
+//                setProgress(currentPage, pageCount);
+//                clearForm();
+//                reviewPage();
+//                if(currentPage == pageCount){
+//                    next.setText(R.string.finish);
+//                }
+//
+//            }
+        });
+
+        //setup back button
+        back.setOnClickListener(v -> {
+//            if(currentPage == pageCount){
+//                next.setText(R.string.next);
+//            }
+//            currentPage--;
+//            setProgress(currentPage, pageCount);
+//            clearForm();
+//
+//            if(currentPage == 6){
+//                displayPicture(pages.get(currentPage - 1));
+//            }
+//            else{
+//                DisplayFormPage.displayPage(pages.get(currentPage - 1), form, NewClientActivity.this);
+//            }
+//            //load previously saved answers if any
+//            loadAnswers(pages.get(currentPage - 1));
+//            if(currentPage == 1){
+//                back.setClickable(false);
+//                back.setVisibility(View.INVISIBLE);
+//                back.setBackgroundColor(Color.DKGRAY);
+//
+//            }
+        });
+        back.setClickable(false);
+        back.setVisibility(View.INVISIBLE);
+
+        back.setBackgroundColor(Color.DKGRAY);
+
+
     }
 
     private void ToolbarButtons(){
