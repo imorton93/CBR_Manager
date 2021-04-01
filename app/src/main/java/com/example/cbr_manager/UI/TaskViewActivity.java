0889750e2aac0e1eb3451c178381135deaa20058
Rakim Zubair <rakimz@sfu.ca>
2021-03-31 17:11:16 -0700

    removing dupe function

--- app/src/main/java/com/example/cbr_manager/UI/TaskViewActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/TaskViewActivity.java
@@ -208,31 +208,6 @@ public class TaskViewActivity extends AppCompatActivity {
 
     }
 
-    public JSONArray cur2Json(Cursor cursor) {
-
-        JSONArray resultSet = new JSONArray();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            int totalColumn = cursor.getColumnCount();
-            JSONObject rowObject = new JSONObject();
-            for (int i = 0; i < totalColumn; i++) {
-                if (cursor.getColumnName(i) != null) {
-                    try {
-                        rowObject.put(cursor.getColumnName(i),
-                                cursor.getString(i));
-                    } catch (Exception e) {
-                        Toast.makeText(TaskViewActivity.this, "Exception Error", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-            resultSet.put(rowObject);
-            cursor.moveToNext();
-        }
-
-        cursor.close();
-        return resultSet;
-    }
-
     private void ToolbarButtons(){
         ImageButton homeBtn = findViewById(R.id.homeButton);
         homeBtn.setOnClickListener(new View.OnClickListener() {
