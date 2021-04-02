9a9098814a2f18d4d2e9c81b32883c795bfce5fe
Karan Sachdeva <karan_sachdeva@sfu.ca>
2021-04-02 19:42:26 +0530

    Set up UI for Social Survey.

---
+++ app/src/main/java/com/example/cbr_manager/UI/BaselineSurvey/SocialSurveyActivity.java
@@ -0,0 +1,23 @@
+package com.example.cbr_manager.UI.BaselineSurvey;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cbr_manager.R;
+
+public class SocialSurveyActivity extends AppCompatActivity {
+
+    public static Intent makeIntent(Context context) {
+        Intent intent = new Intent(context, SocialSurveyActivity.class);
+        return intent;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social_survey);
+    }
+
+}
