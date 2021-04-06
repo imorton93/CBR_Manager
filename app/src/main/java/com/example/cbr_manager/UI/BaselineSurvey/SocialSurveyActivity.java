f4252187a57a5b3aa1319b913f1f8bec06fcd8cd
Karan Sachdeva <karan_sachdeva@sfu.ca>
2021-04-07 01:50:51 +0530

    Stored social survey input.

--- app/src/main/java/com/example/cbr_manager/UI/BaselineSurvey/SocialSurveyActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/BaselineSurvey/SocialSurveyActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cbr_manager.Database.AdminMessageManager;
+import com.example.cbr_manager.Database.Survey;
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.UI.DashboardActivity;
 import com.example.cbr_manager.UI.ProfileActivity;
@@ -21,7 +23,8 @@ import com.example.cbr_manager.UI.TaskViewActivity;
 public class SocialSurveyActivity extends AppCompatActivity {
 
     private Button nextButton, backButton;
-    private RadioGroup valueRadio, independentRadio, participateRadio, disabilityRadio, satisfiedRadio;
+    private RadioGroup valueRadio, independentRadio, participateRadio, disabilityRadio, discriminatedRadio;
+    Survey survey;
 
     public static Intent makeIntent(Context context) {
         Intent intent = new Intent(context, SocialSurveyActivity.class);
@@ -39,7 +42,10 @@ public class SocialSurveyActivity extends AppCompatActivity {
         independentRadio = findViewById(R.id.socialSurveyRadioGroup2);
         participateRadio = findViewById(R.id.socialSurveyRadioGroup3);
         disabilityRadio = findViewById((R.id.socialSurveyRadioGroup4));
-        satisfiedRadio = findViewById((R.id.socialSurveyRadioGroup5));
+        discriminatedRadio = findViewById((R.id.socialSurveyRadioGroup5));
+        survey = (Survey) getIntent().getSerializableExtra("Survey");
+
+
 
         nextButton();
         backButton();
@@ -63,11 +69,59 @@ public class SocialSurveyActivity extends AppCompatActivity {
                 if (!validateEntries())
                     Toast.makeText(SocialSurveyActivity.this, "Please fill all the details", Toast.LENGTH_LONG).show();
                 else {
+                    storeSurveyInput();
                     Intent intent = LivelihoodSurveyActivity.makeIntent(SocialSurveyActivity.this);
+                    Toast.makeText(SocialSurveyActivity.this, Boolean.toString(survey.isIs_valued()), Toast.LENGTH_LONG).show();
+                    intent.putExtra("Survey", survey);
                     startActivity(intent);
                 }
             }
         });
+    }
+
+    private void storeSurveyInput() {
+        int id = valueRadio.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton) findViewById(id);
+        String answer1 = rb.getText().toString();
+        boolean is_valued;
+        if(answer1.equals("No"))
+            is_valued = false;
+        else
+            is_valued = true;
+
+        String answer2 = ((RadioButton)findViewById(independentRadio.getCheckedRadioButtonId())).getText().toString();
+        boolean is_independent;
+        if(answer2.equals("No"))
+            is_independent = false;
+        else
+            is_independent = true;
+
+        String answer3 = ((RadioButton)findViewById(participateRadio.getCheckedRadioButtonId())).getText().toString();
+        boolean is_social;
+        if(answer3.equals("No"))
+            is_social = false;
+        else
+            is_social = true;
+
+        String answer4 = ((RadioButton)findViewById(disabilityRadio.getCheckedRadioButtonId())).getText().toString();
+        boolean is_affected;
+        if(answer4.equals("No"))
+            is_affected = false;
+        else
+            is_affected = true;
+
+        String answer5 = ((RadioButton)findViewById(discriminatedRadio.getCheckedRadioButtonId())).getText().toString();
+        boolean was_discriminated;
+        if(answer5.equals("No"))
+            was_discriminated = false;
+        else
+            was_discriminated = true;
+
+        survey.setIs_valued(is_valued);
+        survey.setIs_independent(is_independent);
+        survey.setIs_social(is_social);
+        survey.setIs_socially_affected(is_affected);
+        survey.setWas_discriminated(was_discriminated);
     }
 
     private void backButton() {
@@ -82,7 +136,7 @@ public class SocialSurveyActivity extends AppCompatActivity {
     private boolean validateEntries() {
         boolean bool = true;
         if (valueRadio.getCheckedRadioButtonId() == -1 || participateRadio.getCheckedRadioButtonId() == -1
-        || satisfiedRadio.getCheckedRadioButtonId() == -1 || independentRadio.getCheckedRadioButtonId() == -1 ||
+        || discriminatedRadio.getCheckedRadioButtonId() == -1 || independentRadio.getCheckedRadioButtonId() == -1 ||
         disabilityRadio.getCheckedRadioButtonId() == -1 ) {
             bool = false;
         }
