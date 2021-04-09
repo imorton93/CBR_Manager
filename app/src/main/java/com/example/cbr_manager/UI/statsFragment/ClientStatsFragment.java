d8770e9caf034a2afbdfc8afff9c25bf3dca5b4c
nabbassi <nora_abbassi-mohadjel@sfu.ca>
2021-04-08 19:21:03 -0700

    got critical graph to show data

--- app/src/main/java/com/example/cbr_manager/UI/statsFragment/ClientStatsFragment.java
+++ app/src/main/java/com/example/cbr_manager/UI/statsFragment/ClientStatsFragment.java
@@ -1,6 +1,5 @@
 package com.example.cbr_manager.UI.statsFragment;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,7 +11,6 @@ import android.view.ViewGroup;
 import com.example.cbr_manager.Database.Client;
 import com.example.cbr_manager.Database.ClientManager;
 import com.example.cbr_manager.R;
-import com.example.cbr_manager.UI.ClientInfoActivity;
 import com.example.cbr_manager.UI.StatsActivity;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -21,11 +19,6 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GridLabelRenderer;
-import com.jjoe64.graphview.helper.StaticLabelsFormatter;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
 
 import java.util.ArrayList;
 
@@ -35,8 +28,10 @@ public class ClientStatsFragment extends Fragment {
     private StatsActivity statsActivity;
     private ClientManager clientManager;
     private static final ArrayList<String> RISK_LEVEL = new ArrayList<String>();
+    private static final ArrayList<String> GOAL = new ArrayList<String>();
 
-    public ClientStatsFragment() {}
+    public ClientStatsFragment() {
+    }
 
     public static ClientStatsFragment newInstance() {
         ClientStatsFragment fragment = new ClientStatsFragment();
@@ -62,89 +57,142 @@ public class ClientStatsFragment extends Fragment {
         RISK_LEVEL.add("High Risk");
         RISK_LEVEL.add("Critical Risk");
 
-        healthDataPoints(view);
-        educationDataPoints(view);
-        socialDataPoints(view);
+        GOAL.add("Health");
+        GOAL.add("Education");
+        GOAL.add("Social");
+
+
+        criticalRiskDataPoints(view);
+//        educationDataPoints(view);
+//        socialDataPoints(view);
         return view;
     }
 
-    private void healthDataPoints(View view){
-        BarChart graph = view.findViewById(R.id.client_health_graph);
-        ArrayList<Integer>  healthRiskDataPoints = getHealthDataPoints();
+    private void criticalRiskDataPoints(View view) {
+        BarChart graph = view.findViewById(R.id.client_critical_graph);
+        ArrayList<Integer> criticalRiskDataPoints = getCriticalDataPoints();
         ArrayList<BarEntry> entries = new ArrayList<>();
 
-        for(int i = 0; i < healthRiskDataPoints.size(); i++){
-            BarEntry barEntry = new BarEntry(i, healthRiskDataPoints.get(i));
+        for (int i = 0; i < criticalRiskDataPoints.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, criticalRiskDataPoints.get(i));
             entries.add(barEntry);
         }
 
-        BarDataSet barDataSet = new BarDataSet(entries, "Client Health Risk Levels");
+        BarDataSet barDataSet = new BarDataSet(entries, "Client Critical Risk Levels");
         BarData data = new BarData(barDataSet);
         graph.setData(data);
         XAxis xaxis = graph.getXAxis();
         xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xaxis.setDrawGridLines(false);
-        xaxis.setLabelCount(4);
+        xaxis.setLabelCount(3);
         xaxis.setValueFormatter(new IAxisValueFormatter() {
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                return RISK_LEVEL.get((int)value);
+                return GOAL.get((int) value);
             }
         });
         graph.invalidate();
+
     }
 
-    private ArrayList<Integer> getHealthDataPoints(){
-        ArrayList<Integer> healthRiskDataPoints = new ArrayList<>();
-        int low_count = 0;
-        int medium_count = 0;
-        int high_count = 0;
-        int critical_count = 0;
+    private ArrayList<Integer> getCriticalDataPoints() {
+        ArrayList<Integer> criticalRiskDataPoints = new ArrayList<>();
+        int critical_health_count = 0;
+        int critical_education_count = 0;
+        int critical_social_count = 0;
 
-        for(Client client : clientManager.getClients()){
-            if(client.getHealthRate().equals(RISK_LEVEL.get(0))){
-                low_count++;
-            }else if(client.getHealthRate().equals(RISK_LEVEL.get(1))){
-                medium_count++;
-            }else if(client.getHealthRate().equals(RISK_LEVEL.get(2))){
-                high_count++;
-            }else if(client.getHealthRate().equals(RISK_LEVEL.get(3))){
-                critical_count++;
+        getHealthDataPoints();
+        getEducationDataPoints();
+        getSocialDataPoints();
+
+        getHealthDataPoints().get(critical_health_count);
+        getEducationDataPoints().get(critical_education_count);
+        getSocialDataPoints().get(critical_social_count);
+
+        criticalRiskDataPoints.add(getHealthDataPoints().get(critical_health_count));
+        criticalRiskDataPoints.add(getEducationDataPoints().get(critical_education_count));
+        criticalRiskDataPoints.add(getSocialDataPoints().get(critical_social_count));
+        return criticalRiskDataPoints;
+    }
+
+//
+//    private void healthDataPoints(View view){
+//        BarChart graph = view.findViewById(R.id.client_critical_graph);
+//        ArrayList<Integer>  healthRiskDataPoints = getHealthDataPoints();
+//        ArrayList<BarEntry> entries = new ArrayList<>();
+//
+//        for(int i = 0; i < healthRiskDataPoints.size(); i++){
+//            BarEntry barEntry = new BarEntry(i, healthRiskDataPoints.get(i));
+//            entries.add(barEntry);
+//        }
+//
+//        BarDataSet barDataSet = new BarDataSet(entries, "Client Health Risk Levels");
+//        BarData data = new BarData(barDataSet);
+//        graph.setData(data);
+//        XAxis xaxis = graph.getXAxis();
+//        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xaxis.setDrawGridLines(false);
+//        xaxis.setLabelCount(4);
+//        xaxis.setValueFormatter(new IAxisValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//                return RISK_LEVEL.get((int)value);
+//            }
+//        });
+//        graph.invalidate();
+//    }
+
+        private ArrayList<Integer> getHealthDataPoints () {
+            ArrayList<Integer> healthRiskDataPoints = new ArrayList<>();
+            int low_count = 0;
+            int medium_count = 0;
+            int high_count = 0;
+            int critical_count = 0;
+
+            for (Client client : clientManager.getClients()) {
+                if (client.getHealthRate().equals(RISK_LEVEL.get(0))) {
+                    low_count++;
+                } else if (client.getHealthRate().equals(RISK_LEVEL.get(1))) {
+                    medium_count++;
+                } else if (client.getHealthRate().equals(RISK_LEVEL.get(2))) {
+                    high_count++;
+                } else if (client.getHealthRate().equals(RISK_LEVEL.get(3))) {
+                    critical_count++;
+                }
             }
-        }
-        healthRiskDataPoints.add(low_count);
-        healthRiskDataPoints.add(medium_count);
-        healthRiskDataPoints.add(high_count);
-        healthRiskDataPoints.add(critical_count);
+            healthRiskDataPoints.add(low_count);
+            healthRiskDataPoints.add(medium_count);
+            healthRiskDataPoints.add(high_count);
+            healthRiskDataPoints.add(critical_count);
 
-        return healthRiskDataPoints;
-    }
-
-    private void educationDataPoints(View view){
-        BarChart graph = view.findViewById(R.id.client_education_graph);
-        ArrayList<Integer>  educationRiskDataPoints = getEducationDataPoints();
-        ArrayList<BarEntry> entries = new ArrayList<>();
-
-        for(int i = 0; i < educationRiskDataPoints.size(); i++){
-            BarEntry barEntry = new BarEntry(i, educationRiskDataPoints.get(i));
-            entries.add(barEntry);
+            return healthRiskDataPoints;
         }
 
-        BarDataSet barDataSet = new BarDataSet(entries, "Client Education Risk Levels");
-        BarData data = new BarData(barDataSet);
-        graph.setData(data);
-        XAxis xaxis = graph.getXAxis();
-        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xaxis.setDrawGridLines(false);
-        xaxis.setLabelCount(4);
-        xaxis.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return RISK_LEVEL.get((int)value);
-            }
-        });
-        graph.invalidate();
-    }
+//    private void educationDataPoints(View view){
+//        BarChart graph = view.findViewById(R.id.client_high_graph);
+//        ArrayList<Integer>  educationRiskDataPoints = getEducationDataPoints();
+//        ArrayList<BarEntry> entries = new ArrayList<>();
+//
+//        for(int i = 0; i < educationRiskDataPoints.size(); i++){
+//            BarEntry barEntry = new BarEntry(i, educationRiskDataPoints.get(i));
+//            entries.add(barEntry);
+//        }
+//
+//        BarDataSet barDataSet = new BarDataSet(entries, "Client Education Risk Levels");
+//        BarData data = new BarData(barDataSet);
+//        graph.setData(data);
+//        XAxis xaxis = graph.getXAxis();
+//        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xaxis.setDrawGridLines(false);
+//        xaxis.setLabelCount(4);
+//        xaxis.setValueFormatter(new IAxisValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//                return RISK_LEVEL.get((int)value);
+//            }
+//        });
+//        graph.invalidate();
+//    }
 
     private ArrayList<Integer> getEducationDataPoints(){
         ArrayList<Integer> educationRiskDataPoints = new ArrayList<>();
@@ -172,31 +220,31 @@ public class ClientStatsFragment extends Fragment {
         return educationRiskDataPoints;
     }
 
-    private void socialDataPoints(View view){
-        BarChart graph = view.findViewById(R.id.client_Social_graph);
-        ArrayList<Integer>  socialRiskDataPoints = getSocialDataPoints();
-        ArrayList<BarEntry> entries = new ArrayList<>();
-
-        for(int i = 0; i < socialRiskDataPoints.size(); i++){
-            BarEntry barEntry = new BarEntry(i, socialRiskDataPoints.get(i));
-            entries.add(barEntry);
-        }
-
-        BarDataSet barDataSet = new BarDataSet(entries, "Client Social Status Risk Levels");
-        BarData data = new BarData(barDataSet);
-        graph.setData(data);
-        XAxis xaxis = graph.getXAxis();
-        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xaxis.setDrawGridLines(false);
-        xaxis.setLabelCount(4);
-        xaxis.setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return RISK_LEVEL.get((int)value);
-            }
-        });
-        graph.invalidate();
-    }
+//    private void socialDataPoints(View view){
+//        BarChart graph = view.findViewById(R.id.client_medium_graph);
+//        ArrayList<Integer>  socialRiskDataPoints = getSocialDataPoints();
+//        ArrayList<BarEntry> entries = new ArrayList<>();
+//
+//        for(int i = 0; i < socialRiskDataPoints.size(); i++){
+//            BarEntry barEntry = new BarEntry(i, socialRiskDataPoints.get(i));
+//            entries.add(barEntry);
+//        }
+//
+//        BarDataSet barDataSet = new BarDataSet(entries, "Client Social Status Risk Levels");
+//        BarData data = new BarData(barDataSet);
+//        graph.setData(data);
+//        XAxis xaxis = graph.getXAxis();
+//        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+//        xaxis.setDrawGridLines(false);
+//        xaxis.setLabelCount(4);
+//        xaxis.setValueFormatter(new IAxisValueFormatter() {
+//            @Override
+//            public String getFormattedValue(float value, AxisBase axis) {
+//                return RISK_LEVEL.get((int)value);
+//            }
+//        });
+//        graph.invalidate();
+//    }
 
     private ArrayList<Integer> getSocialDataPoints(){
         ArrayList<Integer> socialRiskDataPoints = new ArrayList<>();
@@ -224,3 +272,5 @@ public class ClientStatsFragment extends Fragment {
         return socialRiskDataPoints;
     }
 }
+
+
