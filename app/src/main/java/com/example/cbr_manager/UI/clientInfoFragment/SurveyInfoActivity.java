1c1ac83c3f8a67f232fc7166c54b8dc370f2c382
dnix <dnix@sfu.ca>
2021-04-08 19:23:37 -0700

    Click Survey updated.

--- app/src/main/java/com/example/cbr_manager/UI/clientInfoFragment/SurveyInfoActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/clientInfoFragment/SurveyInfoActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.cbr_manager.Database.Survey;
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.UI.ClientInfoActivity;
 
@@ -17,7 +18,7 @@ public class SurveyInfoActivity extends AppCompatActivity {
     public static final String R_CLIENT_POS_PASSED_IN = "r_client_POS_passed_in";
 
     public static Intent makeIntent(Context context, long id, int position) {
-        Intent intent =  new Intent(context, ClientInfoActivity.class);
+        Intent intent =  new Intent(context, SurveyInfoActivity.class);
         intent.putExtra(R_CLIENT_ID_PASSED_IN, id);
         intent.putExtra(R_CLIENT_POS_PASSED_IN, position);
         return intent;
