64185355c0bf397de5f6332113547494bafb6096
Rakim Zubair <rakimz@sfu.ca>
2021-04-10 01:10:21 -0700

    baseline notification fix redo

--- app/src/main/java/com/example/cbr_manager/UI/BaselineSurvey/FoodSurveyActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/BaselineSurvey/FoodSurveyActivity.java
@@ -193,7 +193,7 @@ public class FoodSurveyActivity extends AppCompatActivity {
     }
 
     private void badgeNotification(AdminMessageManager adminMessageManager, TextView badge) {
-        int size = adminMessageManager.size();
+        int size = adminMessageManager.numUnread();
 
         if (badge != null) {
             if (size == 0) {
