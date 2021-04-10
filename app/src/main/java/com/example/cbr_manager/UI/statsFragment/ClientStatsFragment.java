0f16645c0552a96c4957e4f0011a284729d5a779
nabbassi <nora_abbassi-mohadjel@sfu.ca>
2021-04-09 17:29:25 -0700

    critical data per zone graph, app crashes when initiall clicked but then works? need to look into

--- app/src/main/java/com/example/cbr_manager/UI/statsFragment/ClientStatsFragment.java
+++ app/src/main/java/com/example/cbr_manager/UI/statsFragment/ClientStatsFragment.java
@@ -1,5 +1,6 @@
 package com.example.cbr_manager.UI.statsFragment;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -11,6 +12,10 @@ import android.view.ViewGroup;
 
 import com.example.cbr_manager.Database.Client;
 import com.example.cbr_manager.Database.ClientManager;
+import com.example.cbr_manager.Database.Visit;
+import com.example.cbr_manager.Forms.FormPage;
+import com.example.cbr_manager.Forms.MultipleChoiceQuestion;
+import com.example.cbr_manager.Forms.QuestionType;
 import com.example.cbr_manager.R;
 import com.example.cbr_manager.UI.StatsActivity;
 import com.github.mikephil.charting.charts.BarChart;
@@ -35,6 +40,7 @@ public class ClientStatsFragment extends Fragment {
     private ClientManager clientManager;
     private static final ArrayList<String> RISK_LEVEL = new ArrayList<String>();
     private static final ArrayList<String> GOAL = new ArrayList<String>();
+    private static final ArrayList<String> ZONES = new ArrayList<String>();
 
     public ClientStatsFragment() {
     }
@@ -57,7 +63,10 @@ public class ClientStatsFragment extends Fragment {
         this.statsActivity = (StatsActivity) getActivity();
         clientManager = ClientManager.getInstance(statsActivity);
         View view = inflater.inflate(R.layout.fragment_client_stats, container, false);
-
+        FormPage page = new FormPage();
+        Resources res = getResources();
+        MultipleChoiceQuestion location = new MultipleChoiceQuestion(getString(R.string.location),getString(R.string.location_newClientForm), QuestionType.DROP_DOWN,res.getStringArray(R.array.locations), true);
+        page.addToPage(location);
         RISK_LEVEL.add("Low Risk");
         RISK_LEVEL.add("Medium Risk");
         RISK_LEVEL.add("High Risk");
@@ -67,10 +76,21 @@ public class ClientStatsFragment extends Fragment {
         GOAL.add("Education");
         GOAL.add("Social");
 
+        ZONES.add("BidiBidi Zone 1");
+        ZONES.add("BidiBidi Zone 2");
+        ZONES.add("BidiBidi Zone 3");
+        ZONES.add("BidiBidi Zone 4");
+        ZONES.add("BidiBidi Zone 5");
+        ZONES.add("Palorinya Basecamp");
+        ZONES.add("Palorinya Zone 1");
+        ZONES.add("Palorinya Zone 2");
+        ZONES.add("Palorinya Zone 3");
+
         criticalRiskDataPoints(view);
         highRiskDataPoints(view);
         mediumRiskDataPoints(view);
         lowRiskDataPoints(view);
+        criticalRiskPerZone(view);
 
         return view;
     }
@@ -122,6 +142,7 @@ public class ClientStatsFragment extends Fragment {
         criticalRiskDataPoints.add(getSocialDataPoints().get(critical_social_count));
         return criticalRiskDataPoints;
     }
+
     private void highRiskDataPoints(View view) {
         BarChart graph = view.findViewById(R.id.client_high_graph);
         ArrayList<Integer> highRiskDataPoints = getHighDataPoints();
@@ -341,6 +362,105 @@ public class ClientStatsFragment extends Fragment {
         socialRiskDataPoints.add(critical_count);
 
         return socialRiskDataPoints;
+    }
+
+    private void criticalRiskPerZone(View view){
+        BarChart graph = view.findViewById(R.id.client_zoneCritical_graph);
+        ArrayList<Integer> criticalRiskDataPoints = getCriticalClientsPerZone();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (int i = 0; i < criticalRiskDataPoints.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, criticalRiskDataPoints.get(i));
+            entries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, "Critical Risk Levels Per Zone");
+        BarData data = new BarData(barDataSet);
+        graph.setData(data);
+
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        graph.setFitBars(true);
+        graph.animateY(2000);
+
+        XAxis xaxis = graph.getXAxis();
+        xaxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xaxis.setDrawGridLines(false);
+        xaxis.setLabelCount(9);
+        xaxis.setLabelRotationAngle(-80);
+        xaxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return ZONES.get((int) value);
+            }
+        });
+        graph.invalidate();
+        graph.getDescription().setEnabled(false);
+    }
+
+    private ArrayList<Integer> getCriticalClientsPerZone(){
+        ArrayList<Integer> riskPerZoneDataPoints = new ArrayList<>();
+        int [] zone_count = new int[9];
+
+        for(Client client : clientManager.getClients()){
+            String location = client.getLocation();
+            String riskLevelHealth = client.getHealthRate();
+            String riskLevelEducation = client.getEducationRate();
+            String riskLevelSocial = client.getSocialStatusRate();
+
+            if(location.equals(ZONES.get(0))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[0]++;
+            }else if(location.equals(ZONES.get(1))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[1]++;
+            }else if(location.equals(ZONES.get(2))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[2]++;
+            }else if(location.equals(ZONES.get(3))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[3]++;
+            }else if(location.equals(ZONES.get(4))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[4]++;
+            }else if(location.equals(ZONES.get(5))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[5]++;
+            }else if(location.equals(ZONES.get(6))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[6]++;
+            }else if(location.equals(ZONES.get(7))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[7]++;
+            }else if(location.equals(ZONES.get(8))
+                    && (riskLevelEducation.equals(RISK_LEVEL.get(3))
+                    || riskLevelHealth.equals(RISK_LEVEL.get(3))
+                    || riskLevelSocial.equals((RISK_LEVEL.get(3))))){
+                zone_count[8]++;
+            }
+        }
+
+        for(int i =0; i < 9; i++){
+            riskPerZoneDataPoints.add(zone_count[i]);
+        }
+        return riskPerZoneDataPoints;
     }
 }
 
