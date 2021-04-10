ed9e6635731a4154ef2b835dd81c86373a571a7b
dnix <dnix@sfu.ca>
2021-04-10 02:25:23 -0700

    referral back button now round.

--- app/src/main/java/com/example/cbr_manager/UI/NewReferralActivity.java
+++ app/src/main/java/com/example/cbr_manager/UI/NewReferralActivity.java
@@ -157,8 +157,6 @@ public class NewReferralActivity extends AppCompatActivity {
                     pageCount = selectedForm.size() + 2;
                     back.setClickable(true);
                     back.setVisibility(View.VISIBLE);
-                    back.setBackground(ContextCompat.getDrawable(this, R.drawable.rounded_form_buttons));
-                    back.setBackgroundColor(Color.parseColor("#6661ED24"));
                     savePage(serviceRequirePage);
                 }
                 else{
